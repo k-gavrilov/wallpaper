60091916c5b09d2412eddb118c233dcd00cb0a82
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2023-11-12 15:58:18 +0100

    Add minimal wallpapers index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate
+  before_action :find_wallpapers, only: [:index]
 
   def index
+  end
+
+  private
+
+  def find_wallpapers
+    @wallpapers = Wallpaper.all
   end
 end
