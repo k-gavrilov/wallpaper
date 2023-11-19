288bedeb7e0b2242e9ccfd421b5cee2f3b6e3ef8
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2023-11-19 16:50:27 +0100

    Add unique index to categories

---
+++ db/migrate/20231119151454_add_unieque_index_to_categories.rb
@@ -0,0 +1,5 @@
+class AddUniequeIndexToCategories < ActiveRecord::Migration[7.1]
+  def change
+    add_index :categories, :name, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_18_140240) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_19_151454) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_18_140240) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "sessions", force: :cascade do |t|
