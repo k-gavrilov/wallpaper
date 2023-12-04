ea164a4c783e251217e40f200a8d0d023f734826
Konstantin Gavrilov <konstantin.gavrilov.92@gmail.com>
2023-12-04 22:50:30 +0100

    Add privacy policy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  layout 'clear'
   skip_before_action :authenticate
   before_action :find_categories, only: [:contact, :license, :privacy_policy]
 
